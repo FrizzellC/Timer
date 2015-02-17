e9780e0e9b186723b639ee83201e048a862787ab
Chantel <chantelfrizzell@hotmail.com>
2015-02-16 20:02:27 -0800

    initial commit

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,8 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var count = 0
+    
+    func updateTime() {
+        count++
+        elapsedTime.text = "\(count)"
+    }
+    
 
+    @IBOutlet weak var elapsedTime: UILabel!
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        elapsedTime.text = "0"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
